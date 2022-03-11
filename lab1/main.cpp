a9ca10095c8861cf500e9a200cef7e96d4fe81c6
s1nce <jjkk900925@gmail.com>
2022-03-11 17:56:02 +0800

    lab1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,7 +4,7 @@
 #include "circular_sector.h"
 #include "sort.h"
 #include <iostream>
-#include <vector>
+#include <vector> //vector
 int main()
 {
   std::vector<Shape*> shapes({new Ellipse(4.0,5.0), new CircularSector(10,90) ,new CircularSector(10,60), new Triangle(0,0,10,0,10,10)});
