b6c9c9e2a0971cc895bb8f877c0da01fcc7c7f6b
s1nce <jjkk900925@gmail.com>
2022-03-11 17:48:16 +0800

    lab1

--- lab1/sort.cpp
+++ lab1/sort.cpp
@@ -2,7 +2,7 @@
 #include<algorithm>
 void sortByArea(std::vector<Shape*>::iterator front,std::vector<Shape*>::iterator end)
 {
-   std::sort(front,end,areaAscendingComparison);
+   std::sort(front,end,areaAscendingComparison);  //test
 }
 bool areaAscendingComparison(Shape *a, Shape *b)
 {
